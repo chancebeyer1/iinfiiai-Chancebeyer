9c82024bbcd5959d6a3e9efed9ceb99739a95bd2
Avocode <avocodedigital@gmail.com>
2025-11-12 00:36:19 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import HeroSection from "../components/home/HeroSection";
-import LogoStrip from "../components/home/LogoStrip";
 import FeatureGrid from "../components/home/FeatureGrid";
 import HowItWorks from "../components/home/HowItWorks";
 import OutboundFeature from "../components/home/OutboundFeature";
 import MetricsTiles from "../components/home/MetricsTiles";
-import Testimonials from "../components/home/Testimonials";
 import Pricing from "../components/home/Pricing";
 import CTASection from "../components/home/CTASection";
 import ContactForm from "../components/home/ContactForm";
@@ -14,12 +12,10 @@ export default function Home() {
   return (
     <div className="w-full">
       <HeroSection />
-      <LogoStrip />
       <FeatureGrid />
       <HowItWorks />
       <OutboundFeature />
       <MetricsTiles />
-      <Testimonials />
       <Pricing />
       <CTASection />
       <ContactForm />
